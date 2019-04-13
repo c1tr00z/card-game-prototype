3598868bae7e0a1091a09bded4b296306ed7ce76
c1tr00z <c1tr00z@yandex.ru>
2019-04-14 01:10:01 +0300

    export card balance to csv file

---
+++ Assets/Cards/Code/Editor/CardBalanceExporterEditorTool.cs
@@ -0,0 +1,19 @@
+﻿using c1tr00z.AssistLib.EditorTools;
+using UnityEngine;
+
+namespace c1tr00z.CardPrototype.Cards {
+    [EditorToolName("Card balance exporter tool")]
+    public class CardBalanceExporterEditorTool : EditorTool {
+
+        protected override void DrawInterface() {
+            if (Button("Export")) {
+                var allCardsDBEntries = DB.GetAll<CardDBEntry>();
+                var allCardsBalanceText = string.Join("\r\n",
+                allCardsDBEntries.Select(c => string.Format("\"{0}\",\"{1}\",\"{2}\"", c.name, c.energyPrice, string.Join("|", c.mechanics.Select(m => string.Format("{0}:{1}", m.mechanic.name, m.param)).ToArray()))).ToArray());
+                Debug.Log(allCardsBalanceText);
+
+                FileUtils.SaveTextToFile(PathUtils.Combine(Application.dataPath, "Export", "cardBalanceText.csv"), allCardsBalanceText);
+            }
+        }
+    }
+}
