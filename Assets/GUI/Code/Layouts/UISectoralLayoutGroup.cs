b84eb153f757370c7fc6955a7ea28aa199fb5480
c1tr00z <c1tr00z@yandex.ru>
2019-04-17 13:53:23 +0300

    sectoral layout

---
+++ Assets/GUI/Code/Layouts/UISectoralLayoutGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace c1tr00z.AssistLib.UI {
+    public class UISectoralLayoutGroup : UIRadialLayoutGroup {
+
+        public int sectorAngle = 30;
+
+        public Vector2 positionDelta;
+
+        protected override void Refresh() {
+            var children = GetChildren();
+
+            var count = children.Count;
+
+            var firstAngle = (children.Count - 1) * (-1 * step / 2);
+
+            for (var i = 0; i < children.Count; i++) {
+                var child = children[i];
+                var angle = firstAngle + (i * step);
+                SetPositionByAngleAndRadius(child, angle, true);
+
+                child.anchoredPosition += positionDelta;
+            }
+        }
+    }
+}
