c2baadd2217a8c825cdcfbb302b999deb1e2aed5
c1tr00z <c1tr00z@yandex.ru>
2019-04-21 03:10:23 +0300

    Add battle log

---
+++ Assets/Battle/Code/Log/BattleLog.cs
@@ -0,0 +1,35 @@
+﻿using c1tr00z.CardPrototype.Cards;
+using System.Collections.Generic;
+
+namespace c1tr00z.CardPrototype.Battle {
+    public class BattleLog : BehaviourSingleton<BattleLog> {
+
+        public static event System.Action logUpdated;
+
+        private List<BattleLogEntry> _entries = new List<BattleLogEntry>();
+
+        public List<BattleLogEntry> entries {
+            get {
+                return _entries;
+            }
+        }
+
+        //public List<LogEntry>
+
+        private void OnEnable() {
+            Card.cardPlayed += Card_cardPlayed;
+        }
+
+        private void OnDisable() {
+            Card.cardPlayed -= Card_cardPlayed;
+        }
+
+        private void Card_cardPlayed(Card card, Characters.CharacterBase character) {
+            entries.Add(new BattleLogEntry {
+                card = card,
+                character = character
+            });
+            logUpdated.SafeInvoke();
+        }
+    }
+}
---
+++ Assets/Battle/Code/Log/BattleLogEntry.cs
@@ -0,0 +1,9 @@
+﻿using c1tr00z.CardPrototype.Cards;
+using c1tr00z.CardPrototype.Characters;
+
+namespace c1tr00z.CardPrototype.Battle {
+    public class BattleLogEntry {
+        public Card card;
+        public CharacterBase character;
+    }
+}
