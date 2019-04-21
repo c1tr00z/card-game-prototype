eed94aaecf7a69132ae599fcd875b314e2d29e01
c1tr00z <c1tr00z@yandex.ru>
2019-04-21 03:10:52 +0300

    Card utils

---
+++ Assets/Cards/Code/CardUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace c1tr00z.CardPrototype.Cards {
+    public static class CardUtils {
+        
+        public static Color GetColor(this Card card) {
+            return card.dbEntry.mechanics.MaxElement(m => m.param).mechanic.color;
+        }
+
+        public static string GetText(this Card card) {
+            var cardTextString = "";
+            card.dbEntry.mechanics.ForEach(m => cardTextString += (m.description + ";\r\n"));
+            return cardTextString.Trim();
+        }
+    }
+}
