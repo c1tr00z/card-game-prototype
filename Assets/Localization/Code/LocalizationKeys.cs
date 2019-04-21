db42e082c0eaaef26f5b17bffab95a5841769e0b
c1tr00z <c1tr00z@yandex.ru>
2019-04-21 03:09:32 +0300

    localization: characters, others keys

---
+++ Assets/Localization/Code/LocalizationKeys.cs
@@ -0,0 +1,14 @@
+﻿using c1tr00z.AssistLib.Localization;
+using c1tr00z.CardPrototype.Cards;
+using UnityEngine;
+
+namespace c1tr00z.CardPrototype.Localization {
+    public static class LocalizationKeys {
+
+        public static string CARD_PLAYED = "CardPlayed";
+
+        public static string CardPlayedString(Card card) {
+            return string.Format(AssistLib.Localization.Localization.Translate(CARD_PLAYED), ColorUtility.ToHtmlStringRGB(card.GetColor()), card.dbEntry.GetTitle(), card.GetText());
+        }
+    }
+}
