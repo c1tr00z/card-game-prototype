1b7eccd913d81f06a39a64e3381f8efdc061415a
c1tr00z <c1tr00z@yandex.ru>
2019-04-29 23:28:27 +0300

    Game rules template

---
+++ Assets/Other/Code/GameRulesDBEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace c1tr00z.CardPrototype.Other {
+    public class GameRulesDBEntry : DBEntry {
+        
+        public TextAsset LoadRulesText() {
+            return this.Load<TextAsset>(AssistLib.Localization.Localization.currentLanguage.ToString());
+        }
+    }
+}
